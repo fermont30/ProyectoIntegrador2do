eecf1f7f540e0e2a319c1165a4c0100b733fc53b
fermont30 <lfq.montoya@yavirac.edu.ec>
2023-01-22 20:44:34 -0500

    Users

---
+++ Users/usersdb.sql
@@ -0,0 +1,9 @@
+-- Crear la base de datos usersdb;
+
+CREATE TABLE users (
+	id serial PRIMARY KEY,
+	fullname VARCHAR ( 100 ) NOT NULL,
+	username VARCHAR ( 50 ) NOT NULL,
+	password VARCHAR ( 255 ) NOT NULL,
+	email VARCHAR ( 50 ) NOT NULL
+);
