bd66424fed7412a33ab2911bb1d023960a367b14
fermont30 <lfq.montoya@yavirac.edu.ec>
2023-02-04 15:36:21 -0500

    terminado

--- integrador/apps/pedidos/urls.py
+++ integrador/apps/pedidos/urls.py
@@ -1,7 +1,8 @@
 from ...helpers.models import Path
-from .views import index, add_pedido
+from .views import index, add_pedido, gracias
+
 url_patterns = [
     Path('/', index, ['GET']),
     Path('/add', add_pedido, ['POST']),   
-
+    Path('/gracias', gracias, ['GET']),
 ]
--- integrador/apps/pedidos/views.py
+++ integrador/apps/pedidos/views.py
@@ -11,9 +11,8 @@ port =      5432
 conn = psycopg2.connect(host=host, database=database,
                    user=username, password=password, port=port)
 
-
-def index():
-    #Para registrar    
+# index
+def index():      
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)   
     s1 = "SELECT * FROM productos"
     cur.execute(s1)
@@ -22,28 +21,30 @@ def index():
     response = make_response(template)
     return response
 
- #def perfil():
+# pedido
+def gracias():
     use = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     if 'loggedin' in session:
         use.execute('SELECT * FROM users WHERE id = %s', [session['id']])
         usuarios = use.fetchone()
         # Show the profile page with account info
-        return render_template('pedidos/index.html', usuarios=usuarios)
+        return render_template('pedidos/gracias.html', usuarios=usuarios)
     # User is not loggedin redirect to login page    
     template = render_template("users/login.html")
     response = make_response(template)
     return response  
 
-
+# añadir pedido
 def add_pedido():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     if request.method == 'POST':
         idproductos = request.form['idproductos']        
         cantidad    = request.form['cantidad']       
         cur.execute("INSERT INTO pedidos (idproductos, cantidad) VALUES (%s,%s)", (idproductos, cantidad))
-        conn.commit()
-        flash('Pedido agregado con Exito!!')
+        conn.commit()       
         return redirect(url_for('pedidos.index'))
+
+
 
 
 
